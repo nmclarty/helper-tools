a04d21caa8d89fcf5397b8f5a98c50ff87d08461
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2025-12-24 01:18:50 -0800

    Add sops-podman

---
+++ nix/modules/nixos/sops-podman.nix
@@ -0,0 +1,36 @@
+{ lib, config, perSystem, ... }:
+with lib;
+let
+  cfg = config.services.sops-podman;
+in
+{
+  options.services.sops-podman = {
+    enable = mkEnableOption "Enable loading sops secrets to podman.";
+    settings = {
+      sopsFile = mkOption {
+        type = types.str;
+        description = "The path the the encrypted sops file to be loaded from.";
+      };
+      podmanConnection = mkOption {
+        type = types.str;
+        default = "unix:///run/podman/podman.sock";
+        description = "The podman connection string to use.";
+      };
+    };
+  };
+  config = mkIf cfg.enable {
+    sops.secrets."sops-podman.yaml" = {
+      restartUnits = [ "sops-podman.service" ];
+      sopsFile = cfg.settings.sopsFile;
+      key = "";
+    };
+    systemd.services.sops-podman = {
+      description = "Load system managed secrets into the podman secret store";
+      before = [ "network-online.target" ];
+      wantedBy = [ "network-online.target" ];
+      serviceConfig = {
+        ExecStart = "${perSystem.nix-helpers.default}/bin/sops_podman -s '${config.sops.secrets."sops-podman.yaml".path}' -p '${cfg.settings.podmanConnection}'";
+      };
+    };
+  };
+}
---
+++ sops_podman/__main__.py
@@ -0,0 +1,51 @@
+"""Simple tool for loading secrets from a YAML file to podman."""
+
+from argparse import ArgumentParser
+from pathlib import Path
+
+from podman import PodmanClient
+from ruamel.yaml import YAML
+
+
+def __flatten(d: dict, parent: str = "", sep: str = "__") -> dict:
+    items = {}
+    for k, v in d.items():
+        new_key = f"{parent}{sep}{k}" if parent else k
+        if isinstance(v, dict):
+            items.update(__flatten(v, new_key, sep=sep))
+        else:
+            items[new_key] = v
+    return items
+
+
+def main() -> None:
+    """Run the main sops_podman utility."""
+    # cli configuration
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-s", "--secret-file", help="Path to the secret file", required=True
+    )
+    parser.add_argument(
+        "-p",
+        "--podman-connection",
+        help="The podman connection string",
+        required=True,
+    )
+    args = parser.parse_args()
+    yaml = YAML()
+
+    with PodmanClient(base_url=args.podman_connection) as client:
+        if client.ping():
+            current_secrets = client.secrets.list()
+            print(f"Removing {len(current_secrets)} secrets from podman store...")
+            for s in current_secrets:
+                s.remove()
+
+            secrets = __flatten(yaml.load(Path(args.secret_file)))
+            print(f"Adding {len(secrets)} secrets to podman store...")
+            for key, val in secrets.items():
+                client.secrets.create(key, str(val))
+
+
+if __name__ == "__main__":
+    main()
