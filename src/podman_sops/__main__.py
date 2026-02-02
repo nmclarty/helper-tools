cc764a05aa34e4875259bd745dd033c0e57e3850
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2026-02-01 21:36:27 -0800

    Enhanced podman-sops error handling

--- src/podman_sops/__main__.py
+++ src/podman_sops/__main__.py
@@ -5,7 +5,7 @@ import logging
 import subprocess
 
 import ruamel.yaml
-from pydantic import Field, FilePath
+from pydantic import Field, FilePath, ValidationError
 from pydantic_settings import BaseSettings, CliApp, SettingsConfigDict
 
 logger = logging.getLogger(__name__)
@@ -24,7 +24,11 @@ def flatten(d: dict, parent: str = "", sep: str = "__") -> dict:
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(cli_kebab_case=True)
+    model_config = SettingsConfigDict(
+        cli_kebab_case=True,
+        cli_enforce_required=True,
+        cli_prog_name="podman_sops",
+    )
     log_level: str = Field(description="The logging level to use.", default="INFO")
     secret_file: FilePath = Field(description="The secrets YAML file to be loaded.")
 
@@ -51,7 +55,10 @@ class Settings(BaseSettings):
 def main():
     """Sync wrapper function for pydantic-settings since cli_cmd
     needs to be called with CliApp.run to work properly."""
-    CliApp.run(Settings)
+    try:
+        CliApp.run(Settings)
+    except ValidationError as e:
+        logger.error(e)
 
 
 if __name__ == "__main__":
