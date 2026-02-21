0b30166db79feaa8b3522bf248abe08bbe3b4e8e
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2026-02-21 13:35:36 -0800

    Fix system module

--- src/py_motd/modules/system.py
+++ src/py_motd/modules/system.py
@@ -1,44 +1,35 @@
-import json
 import subprocess
+from functools import cached_property
 from typing import Literal
 
 import psutil
-from pydantic import BaseModel, Field, RootModel
+from pydantic import BaseModel, computed_field, field_validator
 
 from py_motd.utils import format_ts, os_version
 
 
-def get_services() -> list[dict]:
-    return json.loads(
-        subprocess.run(
-            [
-                "systemctl",
-                "list-units",
-                "--type",
-                "service",
-                "--all",
-                "--output",
-                "json",
-            ],
-            check=True,
+class Service(BaseModel):
+    name: str
+
+    @computed_field
+    @cached_property
+    def status(self) -> str:
+        return subprocess.run(
+            ["systemctl", "is-active", self.name],
             capture_output=True,
             text=True,
-        ).stdout
-    )
-
-
-class Service(BaseModel):
-    name: str = Field(alias="unit")
-    status: str = Field(alias="sub")
-
-
-Services = RootModel[list[Service]]
+        ).stdout.strip()
 
 
 class System(BaseModel):
     module: Literal["system"]
     name: str = "System"
-    services: list[str]
+    services: list[Service]
+
+    @field_validator("services", mode="before")
+    @classmethod
+    def load_services(cls, names: list[str]):
+        return [Service(name=name) for name in names]
 
     def run(self) -> str:
         output = (
@@ -47,16 +38,10 @@ class System(BaseModel):
             f"  Uptime: {format_ts(psutil.boot_time())}\n"
             f"  Load: {', '.join([str(round(t, 2)) for t in psutil.getloadavg()])}\n"
         )
-        services = Services.model_validate(get_services())
 
         if len(self.services) > 0:
             output += "  Services:\n"
-            for service in self.services:
-                if (
-                    srv := next((s for s in services.root if s.name == service), None)
-                ) is not None:
-                    output += f"    - {service}: {srv.status}\n"
-                else:
-                    output += f"    - {service}: Not found\n"
+            for s in self.services:
+                output += f"    {s.name}: [green]{s.status}[/green]\n"
 
         return output
--- src/py_motd/modules/update.py
+++ src/py_motd/modules/update.py
@@ -31,10 +31,15 @@ class Update(BaseModel):
         """
         with self.file.open() as file:
             data = Data.model_validate_json(file.read())
-
-        return (
+        
+        output = (
             f"[bold]{self.name}:[/bold]\n"
             f"  Nixpkgs: [blue]{'.'.join(data.version.split('.')[:-1])}[/blue]\n"
             f"  Commit: [yellow]{data.commit}[/yellow] ({format_ts(data.age)})\n"
-            f"  Inputs:\n{'\n'.join([f'    - {input.name}: {format_ts(input.age)}' for input in data.inputs])}\n"
         )
+        if len(data.inputs) > 0:
+            output += f"  Inputs:\n"
+            for i in data.inputs:
+                output += f'    {i.name}: {format_ts(i.age)}\n'
+    
+        return output
