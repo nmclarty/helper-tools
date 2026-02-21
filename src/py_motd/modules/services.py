babe07401b1492beb14b6862eb4e75a12c9f191e
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2026-02-21 03:27:39 -0800

    Added services module

---
+++ src/py_motd/modules/services.py
@@ -0,0 +1,11 @@
+from typing import Literal
+
+from pydantic import BaseModel
+
+
+class Services(BaseModel):
+    module: Literal["services"]
+    name: str = "Services"
+
+    def run(self) -> str:
+        return ()
