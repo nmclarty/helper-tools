597b1a5ae3213e627a8ac1a93357ba3dbcc69259
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2026-04-17 02:13:02 -0700

    Replace custom toml file loading with pydantic-settings for py-motd

--- src/py_motd/__main__.py
+++ src/py_motd/__main__.py
@@ -1,11 +1,16 @@
 import logging
-import tomllib
 from asyncio import gather, to_thread
+from os import environ
 from pathlib import Path
 from typing import Annotated, Union
 
-from pydantic import BaseModel, Field, FilePath, ValidationError
-from pydantic_settings import BaseSettings, CliApp, SettingsConfigDict
+from pydantic import BaseModel, Field, ValidationError
+from pydantic_settings import (
+    BaseSettings,
+    CliApp,
+    SettingsConfigDict,
+    TomlConfigSettingsSource,
+)
 from rich.columns import Columns
 from rich.console import Console
 
@@ -19,30 +24,60 @@ console = Console(highlight=False)
 Module = Annotated[Union[Backup, System, Update], Field(discriminator="module")]
 
 
-class Config(BaseModel):
-    modules: list[Module]
+def get_config_path() -> Path:
+    config = Path(environ.get("XDG_CONFIG_HOME", Path.home() / ".config"))
+    return config / "py-motd" / "config.toml"
+
+
+class DisplaySettings(BaseModel):
+    columns: bool = Field(description="Display modules horizontally", default=True)
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(cli_kebab_case=True)
-    log_level: str = Field(description="Logging level to use.", default="WARNING")
-    config_file: FilePath = Field(
-        description="Path to configuration file.",
-        default=Path("~/.config/py-motd/config.toml").expanduser(),
+    model_config = SettingsConfigDict(
+        cli_kebab_case=True,
+        cli_avoid_json=True,
+        toml_file=get_config_path(),
+        env_prefix="PY_MOTD_",
+        env_nested_delimiter="__",
     )
+    log_level: str = Field(description="Logging level to use", default="WARNING")
+    display: DisplaySettings = DisplaySettings()
+    modules: list[Module] = Field(
+        description="Settings and order of modules",
+        default=[],
+    )
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls,
+        init_settings,
+        env_settings,
+        dotenv_settings,
+        file_secret_settings,
+    ):
+        return (
+            init_settings,
+            env_settings,
+            dotenv_settings,
+            file_secret_settings,
+            TomlConfigSettingsSource(settings_cls),
+        )
 
     async def cli_cmd(self) -> None:
         logging.basicConfig(level=self.log_level)
-        with self.config_file.open("rb") as file:
-            config = Config.model_validate(tomllib.load(file))
 
         # run each module in a thread, and then print the ordered output
-        modules = [to_thread(m.run) for m in config.modules]
-        console.print(Columns(await gather(*modules), column_first=True))
+        modules = await gather(*[to_thread(m.run) for m in self.modules])
+        if self.display.columns:
+            console.print(Columns(modules, column_first=True))
+        else:
+            console.print(*modules, end="")
 
 
 def main() -> None:
-    """Sync wrapper function for pydantic-settings since cli_cmd
+    """Wrapper for pydantic-settings since cli_cmd
     needs to be called with CliApp.run to work properly."""
     try:
         CliApp.run(Settings)
